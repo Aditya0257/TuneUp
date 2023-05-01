84ccbcb324764d6e7433c6f76a079268501a4b1d
Aditya0257 <adityasinghthakur.0257@gmail.com>
2023-05-01 14:22:56 +0530

    search feature added

---
+++ static/javascript/searchSong.js
@@ -0,0 +1,24 @@
+function search(){
+    const searchInput=document.getElementById('search-input');
+    let searchedText = searchInput.value;
+    console.log(searchedText);
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `/search?search=${searchedText}`, true);
+  
+    xhr.onload = function() {
+      if (this.status === 200) {
+        // Replace the content of an element with the AJAX response
+        document.getElementById('home_DraggableDiv').innerHTML = this.responseText;
+  
+        // Change the URL displayed in the address bar
+        history.pushState(null, '', '/search');
+        
+      }
+      const heading = document.getElementById('search-heading');
+      heading.innerHTML = searchedText;
+    };
+    xhr.send();
+
+    
+  }
